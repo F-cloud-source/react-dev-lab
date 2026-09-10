4d58c627f25a9ae2c3fb7d398b2a9b498174894b
F-cloud-source <fizcq354@email.com>
2026-09-10 16:32:07 +0500

    New Project Folders

---
+++ src/Action_Button.tsx
@@ -0,0 +1,21 @@
+
+ export function LedgerAction ({Add}) {
+    return (
+        <div>
+            <header>
+
+        
+            
+            <button> Add Buyers</button>
+
+           <button>Delete Buyers</button>
+
+           <button> Update Buyers</button>
+
+            </header>
+      
+
+          
+        </div>
+    );
+}
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+
+
+//import React from 'react';
+import LedgerActions from './Action_Button';
+import './App.css';
+  
+
