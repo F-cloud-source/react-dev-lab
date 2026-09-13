fda538765738a973c3f08765be32c2af756ee389
F-cloud-source <fizcq354@email.com>
2026-09-13 19:02:26 +0500

    Working on it

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,11 @@
-//import {  // type Request, type Response} from 'express';
+import { type Request, type Response } from 'express';
 
 
-import express,  { type Express, type Request , type Response} from 'express';
 
-const server:  Express = express();
-
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }))
-
-server.post('/' , (req: Request, res: Response) => {
-      console.log(req.body)
-      res.json(req.body);
-})
+const app: Express = express():
 
 
-console.log("Server is live");
+// GET method route 
+app.get("./", (req: Request, res: Response) =>{
+      res.send("Get request to the hoempage")
+}) 
